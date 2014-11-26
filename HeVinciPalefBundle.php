1dfba4778c889645e3fdd9df185369a10398a852
Stéphane Klein <stephaneklein221@gmail.com>
2014-11-26 09:25:03 +0100

    Initial plugin structure

---
+++ HeVinciPalefBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HeVinci\PalefBundle;
+
+use Claroline\CoreBundle\Library\PluginBundle;
+
+class HeVinciPalefBundle extends PluginBundle
+{
+    public function hasMigrations()
+    {
+        return false;
+    }
+} 
